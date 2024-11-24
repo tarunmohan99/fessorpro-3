4da266d7b557f4418ded4e02e016c9fc694bec3b
tarunmohan99 <tarunmohan99@gmail.com>
2024-11-25 01:23:09 +0530

    Dispersion trading strategy

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hello')
+# Data manipulation
+
+import numpy as np
+import pandas as pd
+import datetime
+
+# To calculate Greeks
+import mibian
+
+# For Plotting
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.simplefilter("ignore")
+
